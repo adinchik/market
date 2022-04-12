a380276357066526e2f142ba497ba2140f9ac31b
adinchik <1chik.leoprog@gmail.com>
2022-04-12 17:22:53 +0600

    add hw 2

--- src/main/java/ru/geekbrains/march/market/services/ProductService.java
+++ src/main/java/ru/geekbrains/march/market/services/ProductService.java
@@ -2,16 +2,19 @@ package ru.geekbrains.march.market.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.geekbrains.march.market.dtos.CreateNewProductDto;
+import ru.geekbrains.march.market.dtos.ProductDto;
+import ru.geekbrains.march.market.exceptions.ResourceNotFoundException;
 import ru.geekbrains.march.market.entities.Product;
 import ru.geekbrains.march.market.repositories.ProductRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryService categoryService;
 
     public List<Product> findAll() {
         return productRepository.findAll();
@@ -21,10 +24,15 @@ public class ProductService {
         productRepository.deleteById(id);
     }
 
-    public void createNewProduct(CreateNewProductDto createNewProductDto) {
+    public void createNewProduct(ProductDto productDto) {
         Product product = new Product();
-        product.setTitle(createNewProductDto.getTitle());
-        product.setPrice(createNewProductDto.getPrice());
+        product.setTitle(productDto.getTitle());
+        product.setPrice(productDto.getPrice());
+        product.setCategory(categoryService.findByTitle(productDto.getCategoryTitle()).orElseThrow(() -> new ResourceNotFoundException("Категория с названием: " + productDto.getCategoryTitle() + " не найдена")));
         productRepository.save(product);
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
     }
 }
